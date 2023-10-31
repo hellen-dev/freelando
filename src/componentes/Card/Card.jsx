f201f889af992575baeb9a2e7dbaf77d1e6d2f56
hellen-dev <rm98284@fiap.com.br>
2023-10-31 11:06:38 -0300

    configurando tema dinâmico

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,10 +1,13 @@
 import styled from "@emotion/styled";
 
+//quando eu passar para urbitávavel, posso substituir essas props por valores estáticos dentro do arquivo sass
+
 const DivEstilizada = styled.div`
-  padding: 32px;
-  background: #ebeaf9;
-  border: 1px solid #5754ed;
-  border-radius: 16px;
+  padding: ${props => props.theme.espacamentos.l};
+  background: ${props => props.theme.cores.secundarias.a};
+  border: 1px solid;
+  border-color: ${props => props.theme.cores.primarias.a};
+  border-radius: ${props => props.theme.espacamentos.s};
 `;
 
 export const Card = ({ children }) => {
--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -7,13 +7,13 @@ const tema = {
         focus: '',
 
         primarias: {
-            a: '',
+            a: '#5754ED',
             b: '',
             c: '',
         },
 
         secundarias: {
-            a: '',
+            a: '#EBEAF9',
             b: '',
             c: '',
         },
@@ -28,7 +28,12 @@ const tema = {
             a: '',
             b: '',
         },
-    }
+    },
+
+    espacamentos: {
+       s : '16px',
+       l : '32px',
+    },
 }
 
 export const ProvedorTema = ({ children }) => {
